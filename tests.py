55d1907cce49f7fef61a77873579bd7e6a58630c
Mythic534 <tom.simpson733@gmail.com>
2026-05-26 21:40:54 +0100

    test alarm

---
+++ tests.py
@@ -0,0 +1,9 @@
+import database as db
+
+db.init_db()
+
+con = db.connect()
+cur = con.cursor()
+
+cur.execute("INSERT INTO alarms (datetime, name, recurring, recurring_days) VALUES (?, ?, ?, ?)",
+            ("2026-05-26 07:30", "Test", 0, None))
