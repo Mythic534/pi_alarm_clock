0e3475febfb0fe40dbc4059b776c55990f328952
Mythic534 <tom.simpson733@gmail.com>
2026-05-26 21:02:45 +0100

    test alarm

--- database.py
+++ database.py
@@ -12,8 +12,8 @@ recurring_weekdays TEXT,
 enabled INTEGER DEFAULT 1
 );""")
 
-cur.execute("""INSERT INTO alarms (datetime, name, recurring, recurring_weekdays)
-VALUES ('2026-05-26 07:30', 'Test alarm', 0, NULL);""")
+cur.execute("INSERT INTO alarms (datetime, name, recurring, recurring_weekdays) VALUES (?, ?, ?, ?)",
+            ("2026-05-26 07:30", "Test", 0, None))
 
 con.commit()
 con.close()
