d44b64065c08fba2a56e21ee8cd8a7aa5ce510e9
Mythic534 <tom.simpson733@gmail.com>
2026-05-26 21:27:45 +0100

    test alarm

--- alarm.py
+++ alarm.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
 import typer
+import database as db
 
+db.init_db()
 app = typer.Typer()
+
+def manage():
+    alarms = db.get_alarms()
+
+    print("\nID | DATETIME | NAME | RECURRING | RECURRING_DAYS | ENABLED")
+    print("-" * 60)
+
+    for a in alarms:
+        print(f"{a[0]} | {a[1]} | {a[2]} | {a[3]} | {a[4]} | {a[5]}")
 
 @app.command()
 def add(time: str):
@@ -10,7 +21,18 @@ def add(time: str):
     print(f"Adding alarm at {time}")
 
 @app.command()
-def foad(time: str):
-    print(f"Adding foad at {time}")
+def view():
+    print(f"Viewing alarms")
+    manage()
+
+@app.command()
+def disable():
+    print(f"Disabling alarms")
+    manage()
+
+@app.command()
+def remove():
+    print(f"Removing alarms")
+    manage()
 
 app()
--- database.py
+++ database.py
@@ -1,19 +1,33 @@
 import sqlite3
 
-con = sqlite3.connect("alarms.db")
-cur = con.cursor()
+def connect():
+    return sqlite3.connect("alarms.db")
 
-cur.execute("""CREATE TABLE IF NOT EXISTS alarms (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-datetime TEXT,
-name TEXT,
-recurring INTEGER,
-recurring_weekdays TEXT,
-enabled INTEGER DEFAULT 1
-);""")
+def init_db():
+    con = connect()
+    cur = con.cursor()
 
-cur.execute("INSERT INTO alarms (datetime, name, recurring, recurring_weekdays) VALUES (?, ?, ?, ?)",
-            ("2026-05-26 07:30", "Test", 0, None))
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS alarms (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        datetime TEXT,
+        name TEXT,
+        recurring INTEGER,
+        recurring_days TEXT,
+        enabled INTEGER DEFAULT 1
+    );
+    """)
 
-con.commit()
-con.close()
+    con.commit()
+    con.close()
+
+"""cur.execute("INSERT INTO alarms (datetime, name, recurring, recurring_days) VALUES (?, ?, ?, ?)",
+            ("2026-05-26 07:30", "Test", 0, None))"""
+
+def get_alarms():
+    con = connect()
+    cur = con.cursor()
+    cur.execute("SELECT id, datetime, name, recurring, recurring_days, enabled FROM alarms")
+    rows = cur.fetchall()
+    con.close()
+    return rows
