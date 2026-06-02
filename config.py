813e7d38831b0574d35d743aa07d75f8f2dde82c
Mythic534 <tom.simpson733@gmail.com>
2026-06-02 23:38:34 +0100

    backlight auto-dim

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 AUTO_DIM = True
 DIM_AFTER_SECONDS = 30
 BACKLIGHT_BRIGHT = 30
-BACKLIGHT_DIM = 5
+BACKLIGHT_DIM = 1
 BACKLIGHT_PATH = "/sys/class/backlight/11-0045/device/backlight/11-0045/brightness"
